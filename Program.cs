d86c2581f3e67796fe745644cbb5b4b8233dcfe1
erce2021 <83460639+erce2021@users.noreply.github.com>
2022-01-19 08:40:09 -0600

    Clase dia 19

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Clasedia2parte2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* string nombre;
+            //int edad;
+
+           for(int i=1; i<=5; i++)
+            {
+
+                Console.WriteLine("Introduzca el nombre "+i);
+                nombre=Console.ReadLine();
+                Console.WriteLine("Introduzca la edad de "+nombre);
+                Console.ReadLine();
+
+            }//fin del ciclo*/
+
+            string num;
+            int nume,suma =0,promedio=0;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                int[] numeros = new int[5];
+                Console.WriteLine("Introduzca el numero " + i);
+                num = Console.ReadLine();
+                nume = Convert.ToInt32(num);
+
+                suma +=nume;
+                promedio = suma / (i-1);
+            }//fin del ciclo
+            Console.WriteLine("La suma de todos los numeros es: {0}", suma);
+            Console.WriteLine("el promedio de los numeros es: {0}", promedio);
+
+        }
+    }
+}
