7f4b921be11bf10fc6f3450941c64de466ce04b0
erce2021 <83460639+erce2021@users.noreply.github.com>
2022-01-20 07:50:43 -0600

    Clase dia jueves

--- Program.cs
+++ Program.cs
@@ -1,40 +1,57 @@
 ﻿using System;
 
-namespace Clasedia2parte2
+namespace Gastosdia3
 {
     class Program
     {
         static void Main(string[] args)
         {
-           /* string nombre;
-            //int edad;
+            //Crea un arreglo
+            double[] vGastos;
+            //Crea las variables
+            double gasto, totalGasto, diaMayor, gastoMayor;
+            //Un int para los dias 
+            int cantDias;
 
-           for(int i=1; i<=5; i++)
+            Console.WriteLine("Coloque los dias que desea introducir");
+            cantDias = Convert.ToInt32(Console.ReadLine());
+            //Almacena la cantidad de dia en el arreglo , la cantidad de gastos va en dependencia en los dias
+            vGastos = new double[cantDias];
+
+            for(int dia = 1; dia <= cantDias; dia++)
             {
+                Console.WriteLine("Ingrese el gasto del dia " + dia + " Gastos");
+                //Se introducen los gastos
+                gasto = Convert.ToDouble(Console.ReadLine());
+                //Crea un contador que va almacenando la cantidad de los gastos en el arreglo
+                vGastos[dia - 1] = gasto;
+            }
+            
+            //Hacemos una comparacion de cual gasto es mayor 
+            //Inicializa las variables con 0
+            gastoMayor = 0;
+            totalGasto = 0;
+            //Inicializa el dia con 1
+            diaMayor = 1;
 
-                Console.WriteLine("Introduzca el nombre "+i);
-                nombre=Console.ReadLine();
-                Console.WriteLine("Introduzca la edad de "+nombre);
-                Console.ReadLine();
-
-            }//fin del ciclo*/
-
-            string num;
-            int nume,suma =0,promedio=0;
-
-            for (int i = 1; i <= 5; i++)
+            //
+            for(int i = 0; i < cantDias; i++)
             {
-                int[] numeros = new int[5];
-                Console.WriteLine("Introduzca el numero " + i);
-                num = Console.ReadLine();
-                nume = Convert.ToInt32(num);
+                //Evalua el gasto mayor
+                if (vGastos[i] > gastoMayor)
+                {
 
-                suma +=nume;
-                promedio = suma / (i-1);
-            }//fin del ciclo
-            Console.WriteLine("La suma de todos los numeros es: {0}", suma);
-            Console.WriteLine("el promedio de los numeros es: {0}", promedio);
-
+                    gastoMayor = vGastos[i];
+                    diaMayor = i + 1;
+                }
+                //Calculamos el total
+                totalGasto = totalGasto + vGastos[i];
+                //Mostramos los gastos
+                Console.WriteLine("Dia " + (i + 1) + " Total: " + vGastos[i]);
+            }
+            Console.WriteLine($"El gasto mayor es: {gastoMayor}");
+            Console.WriteLine($"El total de gasto es: {totalGasto}");
+            Console.WriteLine($"El dia de mayor gasto es: {diaMayor}");
         }
     }
 }
