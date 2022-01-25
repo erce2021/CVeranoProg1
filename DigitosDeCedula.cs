ff98c4eea3a36247b9cf06dd9c5872af0cbc26b8
erce2021 <83460639+erce2021@users.noreply.github.com>
2022-01-25 08:23:48 -0600

    Add files via upload

---
+++ DigitosDeCedula.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DigitosDeCedula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int numC = 1,
+                numP = 0,
+                numIP = 0,
+                sumIMP = 0;
+
+            while ( numC >= 0 )
+            {
+                Console.WriteLine("Escriba los digitos de su cedula:");
+                numC = int.Parse(Console.ReadLine());
+
+                if ((numC % 2) == 0)
+                {
+                    numP++;
+                    
+                }
+                else
+                {
+                    numIP++;
+                    sumIMP += numIP;
+                    
+                }
+
+            }
+            Console.WriteLine($"Los num pares de la cedula son: {numP}");
+            Console.WriteLine($"Los num impares de la cedula son: {numP}");
+            Console.WriteLine($"La suma de los numeros impares es: {sumIMP}");
+        }
+    }
+}
