fef485df056be255a2f803bd8f6ce5b04b87d703
erce2021 <83460639+erce2021@users.noreply.github.com>
2022-02-02 09:10:27 -0600

    Calculadora y su main
    Clase dia 02 /02 /22

---
+++ Calculadora.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Operador
+{
+    class operador
+    {
+        
+
+        public operador()
+        {
+           // Console.WriteLine("Constructor de la clase operador");
+        }
+
+        int dato1, dato2, resultado = 0;
+        public void setSuma(int a, int b)
+        {
+            this.dato1 = a;
+            this.dato2 = b;
+            this.resultado = dato1 + dato2;
+        }
+        public void setResta(int a, int b)
+        {
+            this.dato1 = a;
+            this.dato2 = b;
+            this.resultado = dato1 - dato2;
+        }
+        public void setDivision(int a, int b)
+        {
+            this.dato1 = a;
+            this.dato2 = b;
+            this.resultado = dato1 / dato2;
+        }
+        public void setMulti(int a, int b)
+        {
+            this.dato1 = a;
+            this.dato2 = b;
+            this.resultado = dato1 * dato2;
+        }
+        public void impResultado()
+        {
+            Console.WriteLine("El resultado es = " + resultado);
+        }
+    }
+}
---
+++ ClassMaindeCalculadora.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operador
+{
+    class Operador
+    {
+        static void Main(string[] args)
+        {
+            operador contador = new operador();
+            Console.WriteLine("Escriba dos numeros: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            contador.setSuma(a,b);
+            contador.impResultado();
+            contador.setResta(a, b);
+            contador.impResultado();
+            contador.setDivision(a, b);
+            contador.impResultado();
+            contador.setMulti(a, b);
+            contador.impResultado();
+        }
+
+    }
+}
