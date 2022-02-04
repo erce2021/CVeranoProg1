c9cab334b255b9cca7c5857604316f8c202280a6
erce2021 <83460639+erce2021@users.noreply.github.com>
2022-02-04 09:22:22 -0600

    clase dia 4

--- Program.cs
+++ Program.cs
@@ -1,44 +1,66 @@
 ﻿using System;
 
-namespace Prueba_1
+namespace App
 {
-    class Program
+    class EmpleadoBaseMasComision
     {
-        static void Main(string[] args)
+        private string primerNombre;
+        private string apellidoPaterno;
+        private string numSegSocial;
+        private decimal ventasBrutas;
+        private decimal tarifaComision;
+        private decimal salarioBase;
+
+        public EmpleadoBaseMasComision(string nombre, string apellido, string nss, decimal ventas, decimal tarifa, decimal salario)
         {
-            string linea;
-            int y =1;
-            int NPar=0, NImp=0;
-            int SumImp = 0;
-
-
-            Console.WriteLine("Digite su Cedula (Sin caraceres): ");
-            Console.WriteLine("Ingrese -1 al finalizar. ");
-            
-            while (y>=0){
-                y = int.Parse(Console.ReadLine());
-            }
-
-
-            for (int i = 1; i <= 10; i++)
-            {
-                if (y % 2 == 0)
-                {
-                    NPar = NPar + 1;
-                }
-                else
-                    SumImp =SumImp + y;
-                Console.WriteLine();
-            }
-
-            Console.WriteLine("La cantidad de numeros pares es: "+NPar);
-            Console.WriteLine("La suma de numeros Impares es: " + SumImp);
-
-
-
-
-
+            primerNombre = nombre;
+            apellidoPaterno = apellido;
+            numSegSocial = nss;
+            ventasBrutas = ventas;
+            tarifaComision = tarifa;
+            salarioBase = salario;
 
         }
+        public string PrimerNombre
+        {
+            get { return primerNombre; }
+            set { PrimerNombre = primerNombre; }
+        }
+        public string ApellidoPaterno
+        {
+            get { return apellidoPaterno; }
+            set { ApellidoPaterno = apellidoPaterno; }
+        }
+        public string NumSegSocial
+        {
+            get { return numSegSocial; }
+            set { NumSegSocial = numSegSocial; }
+        }
+        public decimal VentasBrutas
+        {
+            get { return ventasBrutas; }
+            set { VentasBrutas = (value < 0) ? 0 : value; }
+        }
+        public decimal TarifaComision
+        {
+            get { return tarifaComision; }
+            set { TarifaComision = (value > 0 && value < 1) ? value : 0; }
+        }
+        public decimal SalarioBase
+        {
+            get { return salarioBase; }
+            set { SalarioBase = (value < 0) ? 0 : value; }
+        }
+
+        public decimal Ingresos()
+        {
+            return SalarioBase + (TarifaComision * VentasBrutas);
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+
     }
 }
