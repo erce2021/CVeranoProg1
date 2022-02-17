d0efdca3f12bcb68f94d7ae4c3e6caedc11e94a7
erce2021 <83460639+erce2021@users.noreply.github.com>
2022-02-17 08:49:47 -0600

    Clase dia 17 de febrero

---
+++ Ejemplo2.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class frmventana
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbop = new System.Windows.Forms.ComboBox();
+            this.txtnum2 = new System.Windows.Forms.TextBox();
+            this.rbtn1 = new System.Windows.Forms.RadioButton();
+            this.rbtn2 = new System.Windows.Forms.RadioButton();
+            this.txtnum1 = new System.Windows.Forms.TextBox();
+            this.listadvance = new System.Windows.Forms.ListBox();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(59, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(279, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculadora";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numero 1";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Numero 2";
+            this.label3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // cmbop
+            // 
+            this.cmbop.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbop.FormattingEnabled = true;
+            this.cmbop.Location = new System.Drawing.Point(328, 108);
+            this.cmbop.Name = "cmbop";
+            this.cmbop.Size = new System.Drawing.Size(100, 21);
+            this.cmbop.TabIndex = 3;
+            this.cmbop.SelectedIndexChanged += new System.EventHandler(this.Cmbop_SelectedIndexChanged);
+            // 
+            // txtnum2
+            // 
+            this.txtnum2.Location = new System.Drawing.Point(149, 151);
+            this.txtnum2.Name = "txtnum2";
+            this.txtnum2.Size = new System.Drawing.Size(137, 20);
+            this.txtnum2.TabIndex = 5;
+            this.txtnum2.TextChanged += new System.EventHandler(this.Txtnum2_TextChanged);
+            // 
+            // rbtn1
+            // 
+            this.rbtn1.AutoSize = true;
+            this.rbtn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtn1.Location = new System.Drawing.Point(328, 73);
+            this.rbtn1.Name = "rbtn1";
+            this.rbtn1.Size = new System.Drawing.Size(83, 24);
+            this.rbtn1.TabIndex = 6;
+            this.rbtn1.TabStop = true;
+            this.rbtn1.Text = "Basicas";
+            this.rbtn1.UseVisualStyleBackColor = true;
+            this.rbtn1.CheckedChanged += new System.EventHandler(this.Rbtn1_CheckedChanged);
+            // 
+            // rbtn2
+            // 
+            this.rbtn2.AutoSize = true;
+            this.rbtn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtn2.Location = new System.Drawing.Point(484, 73);
+            this.rbtn2.Name = "rbtn2";
+            this.rbtn2.Size = new System.Drawing.Size(106, 24);
+            this.rbtn2.TabIndex = 7;
+            this.rbtn2.TabStop = true;
+            this.rbtn2.Text = "Avanzadas";
+            this.rbtn2.UseVisualStyleBackColor = true;
+            this.rbtn2.CheckedChanged += new System.EventHandler(this.Rbtn2_CheckedChanged);
+            // 
+            // txtnum1
+            // 
+            this.txtnum1.Location = new System.Drawing.Point(149, 108);
+            this.txtnum1.Name = "txtnum1";
+            this.txtnum1.Size = new System.Drawing.Size(137, 20);
+            this.txtnum1.TabIndex = 4;
+            this.txtnum1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // listadvance
+            // 
+            this.listadvance.FormattingEnabled = true;
+            this.listadvance.Location = new System.Drawing.Point(484, 108);
+            this.listadvance.Name = "listadvance";
+            this.listadvance.Size = new System.Drawing.Size(120, 95);
+            this.listadvance.TabIndex = 8;
+            this.listadvance.SelectedIndexChanged += new System.EventHandler(this.Listadvance_SelectedIndexChanged);
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btncalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalcular.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btncalcular.Location = new System.Drawing.Point(277, 258);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(89, 38);
+            this.btncalcular.TabIndex = 9;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = false;
+            this.btncalcular.Click += new System.EventHandler(this.Btncalcular_Click);
+            // 
+            // frmventana
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(643, 367);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.listadvance);
+            this.Controls.Add(this.rbtn2);
+            this.Controls.Add(this.rbtn1);
+            this.Controls.Add(this.txtnum2);
+            this.Controls.Add(this.txtnum1);
+            this.Controls.Add(this.cmbop);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "frmventana";
+            this.Text = "Este es un ejemplo";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbop;
+        private System.Windows.Forms.TextBox txtnum2;
+        private System.Windows.Forms.RadioButton rbtn1;
+        private System.Windows.Forms.RadioButton rbtn2;
+        private System.Windows.Forms.TextBox txtnum1;
+        private System.Windows.Forms.ListBox listadvance;
+        private System.Windows.Forms.Button btncalcular;
+    }
+}
---
+++ Ejemplo2.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class frmventana : Form
+    {
+        
+
+        public frmventana()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            cmbop.Items.Clear();
+            cmbop.Items.Add("Sumar");
+            cmbop.Items.Add("Restar");
+
+            listadvance.Items.Clear();
+            listadvance.Items.Add("Multiplicacion");
+            listadvance.Items.Add("Division");
+            listadvance.TabIndex = 0;
+
+            cmbop.Enabled = true;
+            listadvance.Enabled = true;
+        }
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Txtnum2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private void Cmbop_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Rbtn1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbtn1.Checked == true)
+            {
+                cmbop.Enabled = true;
+                listadvance.Enabled = false;
+
+
+            }
+
+        }
+
+        private void Rbtn2_CheckedChanged(object sender, EventArgs e)
+        {
+
+            cmbop.Enabled = false;
+            listadvance.Enabled = true;
+            listadvance.SelectedIndex = 0;
+        }
+
+        private void Listadvance_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btncalcular_Click(object sender, EventArgs e)
+        {
+            double n1, n2, r;
+            n1 = Convert.ToDouble(txtnum1.Text);
+            n2 = Convert.ToDouble(txtnum2.Text);
+
+            if (cmbop.Enabled == true)
+            {
+                if (cmbop.SelectedItem.ToString() == "Sumar") r = n1 + n2;
+                else r = n1 - n2;
+                MessageBox.Show("El resultado es " + r.ToString(), "Respuesta");
+            }
+
+            if (listadvance.Enabled == true)
+            {
+                if (listadvance.SelectedItem.ToString() == "Multiplicar") r = n1 * n2;
+                else r = n1 / n2;
+                MessageBox.Show("El resultado es " + r.ToString(), "Respuesta");
+            }
+        }
+    }
+}
