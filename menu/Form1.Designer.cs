1982be56937009e1e64962b253624082df0cceb9
erce2021 <83460639+erce2021@users.noreply.github.com>
2022-02-22 08:54:55 -0600

    Clase dia 22

---
+++ menu/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace menu
+{
+    partial class MenuPruebaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.negroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.azulToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rojoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.verdeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fuenteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.courierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comicToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.negritaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cursivaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mostrarLabel = new System.Windows.Forms.Label();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.formatoToolStripMenuItem});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(454, 24);
+            this.menu.TabIndex = 0;
+            this.menu.Text = "menuStrip1";
+            this.menu.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.AcercaDeToolStripMenuItem_ItemClicked);
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.acercaDeToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.acercaDeToolStripMenuItem.Text = "Acerca de";
+            this.acercaDeToolStripMenuItem.Click += new System.EventHandler(this.AcercaDeToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.SalirToolStripMenuItem_Click);
+            // 
+            // formatoToolStripMenuItem
+            // 
+            this.formatoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.colorToolStripMenuItem,
+            this.fuenteToolStripMenuItem});
+            this.formatoToolStripMenuItem.Name = "formatoToolStripMenuItem";
+            this.formatoToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.formatoToolStripMenuItem.Text = "Formato";
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.negroToolStripMenuItem,
+            this.azulToolStripMenuItem,
+            this.rojoToolStripMenuItem,
+            this.verdeToolStripMenuItem});
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.colorToolStripMenuItem.Text = "Color";
+            // 
+            // negroToolStripMenuItem
+            // 
+            this.negroToolStripMenuItem.Name = "negroToolStripMenuItem";
+            this.negroToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.negroToolStripMenuItem.Text = "Negro";
+            this.negroToolStripMenuItem.Click += new System.EventHandler(this.NegroToolStripMenuItem_Click);
+            // 
+            // azulToolStripMenuItem
+            // 
+            this.azulToolStripMenuItem.Name = "azulToolStripMenuItem";
+            this.azulToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.azulToolStripMenuItem.Text = "Azul";
+            this.azulToolStripMenuItem.Click += new System.EventHandler(this.AzulToolStripMenuItem_Click);
+            // 
+            // rojoToolStripMenuItem
+            // 
+            this.rojoToolStripMenuItem.Name = "rojoToolStripMenuItem";
+            this.rojoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.rojoToolStripMenuItem.Text = "Rojo";
+            this.rojoToolStripMenuItem.Click += new System.EventHandler(this.RojoToolStripMenuItem_Click);
+            // 
+            // verdeToolStripMenuItem
+            // 
+            this.verdeToolStripMenuItem.Name = "verdeToolStripMenuItem";
+            this.verdeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.verdeToolStripMenuItem.Text = "Verde";
+            this.verdeToolStripMenuItem.Click += new System.EventHandler(this.VerdeToolStripMenuItem_Click);
+            // 
+            // fuenteToolStripMenuItem
+            // 
+            this.fuenteToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.timesToolStripMenuItem,
+            this.courierToolStripMenuItem,
+            this.comicToolStripMenuItem,
+            this.negritaToolStripMenuItem,
+            this.cursivaToolStripMenuItem});
+            this.fuenteToolStripMenuItem.Name = "fuenteToolStripMenuItem";
+            this.fuenteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.fuenteToolStripMenuItem.Text = "Fuente";
+            // 
+            // timesToolStripMenuItem
+            // 
+            this.timesToolStripMenuItem.Name = "timesToolStripMenuItem";
+            this.timesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.timesToolStripMenuItem.Text = "Times New Roman";
+            this.timesToolStripMenuItem.Click += new System.EventHandler(this.TimesNewRomanToolStripMenuItem_Click);
+            // 
+            // courierToolStripMenuItem
+            // 
+            this.courierToolStripMenuItem.Name = "courierToolStripMenuItem";
+            this.courierToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.courierToolStripMenuItem.Text = "Curier";
+            this.courierToolStripMenuItem.Click += new System.EventHandler(this.CourierToolStripMenuItem_Click);
+            // 
+            // comicToolStripMenuItem
+            // 
+            this.comicToolStripMenuItem.Name = "comicToolStripMenuItem";
+            this.comicToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.comicToolStripMenuItem.Text = "Comic Sans";
+            this.comicToolStripMenuItem.Click += new System.EventHandler(this.ComicToolStripMenuItem_Click);
+            // 
+            // negritaToolStripMenuItem
+            // 
+            this.negritaToolStripMenuItem.Name = "negritaToolStripMenuItem";
+            this.negritaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.negritaToolStripMenuItem.Text = "Negrita";
+            this.negritaToolStripMenuItem.Click += new System.EventHandler(this.NegritaToolStripMenuItem_Click);
+            // 
+            // cursivaToolStripMenuItem
+            // 
+            this.cursivaToolStripMenuItem.Name = "cursivaToolStripMenuItem";
+            this.cursivaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cursivaToolStripMenuItem.Text = "Cursiva";
+            this.cursivaToolStripMenuItem.Click += new System.EventHandler(this.CursivaToolStripMenuItem_Click);
+            // 
+            // mostrarLabel
+            // 
+            this.mostrarLabel.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.mostrarLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mostrarLabel.Location = new System.Drawing.Point(12, 53);
+            this.mostrarLabel.Name = "mostrarLabel";
+            this.mostrarLabel.Size = new System.Drawing.Size(442, 56);
+            this.mostrarLabel.TabIndex = 1;
+            this.mostrarLabel.Text = "Use el menu formato para cambiar la apariencia de este texto";
+            this.mostrarLabel.UseCompatibleTextRendering = true;
+            this.mostrarLabel.Click += new System.EventHandler(this.MostrarLabel_Click);
+            // 
+            // MenuPruebaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 234);
+            this.Controls.Add(this.mostrarLabel);
+            this.Controls.Add(this.menu);
+            this.MainMenuStrip = this.menu;
+            this.Name = "MenuPruebaForm";
+            this.Text = "Form1";
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formatoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem negroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem azulToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rojoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem verdeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fuenteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem timesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem courierToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem comicToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem negritaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cursivaToolStripMenuItem;
+        private System.Windows.Forms.Label mostrarLabel;
+    }
+}
+
---
+++ menu/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace menu
+{
+    public partial class MenuPruebaForm : Form
+    {
+        public MenuPruebaForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AcercaDeToolStripMenuItem_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            
+        }
+
+        private void AcercaDeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Éste es un ejemplo\ndel uso de menús.",
+            "Acerca de", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void SalirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void BorrarColor()
+        {
+            // borra todas las marcas de verificación
+            negroToolStripMenuItem.Checked = false;
+            azulToolStripMenuItem.Checked = false;
+            rojoToolStripMenuItem.Checked = false;
+            verdeToolStripMenuItem.Checked = false;
+        }
+
+        private void NegroToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // restablece marcas de verificación para los MenuItems de color
+             BorrarColor();
+            
+            // establece Color a Negro
+            mostrarLabel.ForeColor = Color.Black;
+            negroToolStripMenuItem.Checked = true;
+        }
+
+        private void MostrarLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AzulToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BorrarColor();
+
+            // establece Color a Negro
+            mostrarLabel.ForeColor = Color.Blue;
+            azulToolStripMenuItem.Checked = true;
+        }
+
+        private void RojoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BorrarColor();
+
+            // establece Color a Negro
+            mostrarLabel.ForeColor = Color.Red;
+            rojoToolStripMenuItem.Checked = true;
+        }
+
+        private void VerdeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BorrarColor();
+
+            // establece Color a Negro
+            mostrarLabel.ForeColor = Color.Green;
+            verdeToolStripMenuItem.Checked = true;
+        }
+        private void BorrarFuente()
+        {
+            // borra todas las marcas de verificación
+            timesToolStripMenuItem.Checked = false;
+            courierToolStripMenuItem.Checked = false;
+            comicToolStripMenuItem.Checked = false;
+        }
+
+        private void TimesNewRomanToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BorrarFuente();
+            
+            // establece fuente Times New Roman
+            timesToolStripMenuItem.Checked = true;
+            mostrarLabel.Font = new Font(
+                "Times New Roman", 14, mostrarLabel.Font.Style);
+        }
+
+        private void CourierToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BorrarFuente();
+
+            // establece fuente Courier
+            courierToolStripMenuItem.Checked = true;
+            mostrarLabel.Font = new Font(
+                "Courier", 14, mostrarLabel.Font.Style);
+        }
+
+        private void ComicToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BorrarFuente();
+            
+            // establece fuente Comic Sans
+            comicToolStripMenuItem.Checked = true;
+            mostrarLabel.Font = new Font(
+                "Comic Sans MS", 14, mostrarLabel.Font.Style);
+        }
+
+        private void NegritaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            negritaToolStripMenuItem.Checked = !negritaToolStripMenuItem.Checked;
+            mostrarLabel.Font = new Font(
+            mostrarLabel.Font.FontFamily, 14,
+            mostrarLabel.Font.Style ^ FontStyle.Bold);
+        }
+
+        private void CursivaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            cursivaToolStripMenuItem.Checked = !cursivaToolStripMenuItem.Checked;
+            mostrarLabel.Font = new Font(
+            mostrarLabel.Font.FontFamily, 14,
+            mostrarLabel.Font.Style ^ FontStyle.Italic);
+        }
+    }
+}
